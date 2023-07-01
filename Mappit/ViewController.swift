85aa00c39a12f7b9a860128617efcd644a6e4815
hakankoralturk <hakan@koralturk.com>
2023-07-02 00:18:18 +0300

    Initial Commit

---
+++ Mappit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mappit
+//
+//  Created by Hakan Koraltürk on 2.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
